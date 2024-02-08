629306e466a62322ce2308785411dc3adcd4c92a
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-02-08 19:15:42 +0000

    Fixing some errors.

--- src/hostExpr/scaffold.sh
+++ src/hostExpr/scaffold.sh
@@ -92,9 +92,12 @@ for name in ${names[@]}; do
 	# Start the container
 	lxc-start -n "${name}"
 	# Configure Gel
-	lxc-attach -n "${name}" -u 0 -g 0 -- sh "/root/install.sh"
+	lxc-attach -n "${name}" -u 0 -g 0 -- 'sh "/root/install.sh"'
 	# Show the container name
-	lxc-attach -n "${name}" -u 0 -g 0 -- echo "${name}" > /etc/zsh/.customShellName
+	lxc-attach -n "${name}" -u 0 -g 0 -- "echo \"${name}\" > /etc/zsh/.customShellName"
+	# Disable the SSH server by default
+	lxc-attach -n "${name}" -u 0 -g 0 -- 'systemctl disable sshd'
+	lxc-attach -n "${name}" -u 0 -g 0 -- 'systemctl stop sshd'
 	# Increase the start order
 	startOrder=$(($startOrder+1))
 done
